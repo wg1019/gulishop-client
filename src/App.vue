775b3b9d46ffd8b4253c1274463fc833c10b1cbb
wg1019 <1586632187@qq.com>
2021-10-12 15:36:59 +0800

    项目要开始了

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      项目建好了
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style  scoped>
+
+</style>
